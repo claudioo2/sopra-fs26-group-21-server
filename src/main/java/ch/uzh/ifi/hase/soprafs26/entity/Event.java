22e7940844691cd86efdb4bbb8e5775e663e0df0
Pascal-Trautmann <pascal.trautmann@uzh.ch>
2026-03-28 21:36:14 +0100

    added event entity with most essential attributes

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Event.java
@@ -0,0 +1,148 @@
+package ch.uzh.ifi.hase.soprafs26.entity;
+
+import jakarta.persistence.*;
+
+import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
+
+import java.io.Serializable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+/**
+ * Internal User Representation
+ * This class composes the internal representation of the user and defines how
+ * the user is stored in the database.
+ * Every variable will be mapped into a database field with the @Column
+ * annotation
+ * - nullable = false -> this cannot be left empty
+ * - unique = true -> this value must be unqiue across the database -> composes
+ * the primary key
+ */
+@Entity
+@Table(name = "events")
+public class Event implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    // @Column(nullable = false)
+    // private String name;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private UserStatus isPrivate;
+
+    @Column(nullable = false)
+    private User creator;
+
+    @Column(nullable = false)
+    private List<User> participants;
+
+    /*
+    * Missing fields:
+    * status, category, inviteCode, chat
+    *
+    *  */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public UserStatus getIsPrivate() {
+        return isPrivate;
+    }
+
+    public void setIsPrivate(UserStatus isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+}
